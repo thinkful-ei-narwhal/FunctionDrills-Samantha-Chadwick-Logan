da0b554cb38fa3cf136c9ed2af7045e800fe7dc3
Logan Stribling <logan.stribling@gmail.com>
2020-03-16 18:35:23 -0600

    "completed functionOne, started work on functionTwo"

--- functionOne.js
+++ functionOne.js
@@ -1,1 +1,28 @@
-console.log(`Hi, my name is Logan and I'm 27.`);
+function getYearOfBirth(age) {
+  return 2020 - age;
+}
+
+function createGreeting(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new error("arguments invalid");
+  }
+
+  if (age < 0) {
+    throw new error("age cannot be naegative");
+  }
+
+  if (typeof age !== "number") {
+    throw new TypeError("age must be a number");
+  }
+
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old.
+I was born in ${yob}.`;
+}
+
+try {
+  const greeting1 = createGreeting("rich", 18);
+  console.log(greeting1);
+} catch (e) {
+  console.error(e.message);
+}
